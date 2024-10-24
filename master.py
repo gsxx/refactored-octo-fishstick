81a74cc3d8b62cc84af6d1002ccf8d255c2e5c21
gabrieldosmiluno <gabrieldosmiluno@gmail.com>
2024-10-24 21:01:30 +0000

    hola

---
+++ master.py
@@ -0,0 +1,13 @@
+nombre='Gabriel'
+print('ingrese su nombre')
+nombre=input()
+print('hola '+nombre)
+string=nombre
+booleano=True or False
+contador1=0
+
+while contador1<20:
+    contador1=contador1+1
+    for contador2 in range(20):
+        if (contador1%2==0) and (contador2%2==0):
+            print(contador1,' ',contador2)
--- while.py
+++ while.py
@@ -4,4 +4,3 @@ while condicion:
  contador=contador+1
  condicion=contador<10
  print(contador)
-
