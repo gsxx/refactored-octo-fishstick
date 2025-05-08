6d5bd364922b092d1d4b81ccba00a9e57b342166
gsxx <gabrieldosmiluno@gmail.com>
2025-05-08 19:47:09 +0000

    colores

--- main.js
+++ main.js
@@ -9,3 +9,31 @@ variableBoolean = false
 let variableNull = null
 
 let variableLista = ['martin iribarren chernan al432', "gabrielf.sanchez@bue.edu.ar"]
+
+//
+
+let nombredelavaiable = "valor" 
+let variablenumerica = 1.09
+let variableboolean = true
+let variablenull = null
+
+
+
+
+
+
+
+
+let variable = 40+30
+
+console.log(variable)
+
+variable = "Hola soy gabriel"
+
+console.log(variable)
+
+let rojo = 255 //f15*16 f15
+let verde = 255 //2*100+5*10+5*1
+let azul = 255
+let blanco = [rojo,verde,azul]
+print(blanco[0])
