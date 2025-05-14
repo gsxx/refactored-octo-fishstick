bba43c592166fc6eb7869588e90ed76756ec80b1
gsxx <gabrieldosmiluno@gmail.com>
2025-05-14 11:31:04 +0000

    entrada

--- 5/main.py
+++ 5/main.py
@@ -1,2 +1,18 @@
-var=True
-print(var)
+#matiaslopez23
+print("ingrese un numero")
+numero1=int(input())
+#print("ingrese otro numero")
+numero2=int(input("ingrese otro numero"))
+suma=(numero1 + numero2)
+print("la suma de estos numeros da ",suma)
+menor = numero1 < numero2
+mayor = numero1 > numero2
+igual = numero1 == numero2
+#igual ==
+#print("tal es mayor que tal",suma)
+if mayor:  
+    print("numero1 es mayor que numero2")
+if igual:
+    print("numero1 es igual que numero2")
+if menor:  
+    print("numero1 es menor que numero2")
