8f3767eb816933f2a57bd935afa4ae5307c6d542
gsxx <gabrieldosmiluno@gmail.com>
2025-06-04 11:56:22 +0000

    tuplas

---
+++ 5/4-6/main.py
@@ -0,0 +1,18 @@
+'''
+variables
+print
+if
+while
+for
+def
+'''
+
+
+
+
+tuplas = ("los elementos de las tuplas van entre () ")
+tuplas[0]="a"
+listas = ["elemento de la lista 1","elemento de la lista 2"]
+listas[1]=listas[0]
+listas[0]="elemento de la lista 0"
+print(listas)
