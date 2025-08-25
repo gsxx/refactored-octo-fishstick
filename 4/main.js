23f666919fc2405678242e117120a4117c23db3f
gsxx <gabrieldosmiluno@gmail.com>
2025-08-25 20:04:05 +0000

    25 de agosto

--- 4/main.js
+++ 4/main.js
@@ -1,5 +1,8 @@
 //crea una variable que sume dos numeros
 let variable =1+1
+console.log(variable)
 //crea una variable que contenga tu nombre como valor
 let nombre = "gabriel"
 // crea una variable que sume tu nombre y tu apellido
+let nombreCompleto = nombre + " sanchez"
+console.log(nombreCompleto)
