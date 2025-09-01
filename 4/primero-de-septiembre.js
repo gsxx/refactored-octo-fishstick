3fd7cb7a960d55ed8870776d8092466a2d5b3734
gsxx <gabrieldosmiluno@gmail.com>
2025-09-01 20:17:52 +0000

    1-9

---
+++ 4/primero-de-septiembre.js
@@ -0,0 +1,6 @@
+function sumar(atributo1,atributo2){
+    let variableDevuelta = atributo1 + atributo2
+    return variableDevuelta
+}
+let llamar_a_la_funcion= sumar(1,2)
+console.log(llamar_a_la_funcion)
