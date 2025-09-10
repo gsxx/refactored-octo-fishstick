a52dc3d29af7144d213357ae08628afbde81d02f
gsxx <gabrieldosmiluno@gmail.com>
2025-09-10 11:02:14 +0000

    10 de septiembre

--- 4/8-9.js
+++ 4/8-9.js
@@ -1,6 +1,7 @@
-let palabras = ["manzana", "banana", "cereza", "pera"];
+let palabras = ["manzana", "banana", 
+    "cereza", "pera"];
 let ultimaPalabra = palabras[palabras.length - 1];
-//console.log(ultimaPalabra);
+console.log(ultimaPalabra);
 palabras.forEach(element => {
     element = "la "+element
     console.log(element) 
