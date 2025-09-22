a73d3610b3a3eaf96dbae24f6ab891482c0c725e
gsxx <gabrieldosmiluno@gmail.com>
2025-09-22 19:47:26 +0000

    22

---
+++ 4/22-9.js
@@ -0,0 +1,23 @@
+class NombreDeLaClase{
+    constructor(atributo){
+        this.propiedad=atributo
+    }
+    metodo(){
+        console.log(this.propiedad)
+    }
+}
+let instancia = new NombreDeLaClase("valor")
+//instancia.metodo()
+let variable = instancia.propiedad
+
+class Persona{
+    constructor(nombre,){
+        this.nombre=nombre
+    }
+    presentarse(){
+        console.log("mi nombre es",this.nombre)
+    }
+}
+let gabriel = new Persona("Gabriel")
+gabriel.presentarse()
+//Ejercicio: Agregar mas atributos a la clase y crear una instancia con sus propios nombres
