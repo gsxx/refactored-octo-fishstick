0c06b08e59e79ef0110bdc1c775e2a4321d74d80
gsxx <gabrieldosmiluno@gmail.com>
2025-09-29 19:34:49 +0000

    ghjk

---
+++ 4/29-9.js
@@ -0,0 +1,23 @@
+class Padre{
+    constructor(nombre,apellido){
+        this.nombre=nombre
+        this.apellido=apellido
+    }
+}
+class Madre{
+    constructor(nombre){
+        this.nombre=nombre
+    }
+}
+class Hijo extends Madre{
+    constructor(nombre,apellido,año){
+        super(nombre)
+        this.padre=new Padre(nombre,apellido)
+        this.año=año
+    }
+    nombreCompleto(){
+        console.log("mi nombre es "+this.nombre+" "+this.padre.apellido)
+    }
+}
+let gabriel = new Hijo("Gabriel","Sanchez","Cuarto")
+gabriel.nombreCompleto()
